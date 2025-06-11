4d1a76930d83702c3f14ed26c35c1b538f65cc6b
DigiTran Technologies <sukesh.shetty@digitranin.com>
2025-06-11 14:49:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/digitranin.github.io/' // This should match your GitHub repo name
-});
+  base: '/', // 👈 For user site (digitranin.github.io)
+})
